5425377c7853d64845c908a3e6cc3e95a554777e
Andriy Veremyeyev <belyybrat@gmail.com>
2021-03-19 14:16:27 -0700

    Refactor mapping of footer items

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -2,19 +2,23 @@ import React from "react";
 import { Grid, Link } from "@material-ui/core";
 import GitHubIcon from "@material-ui/icons/GitHub";
 import LinkedInIcon from "@material-ui/icons/LinkedIn";
-import EmailIcon from "@material-ui/icons/Email";
 import FacebookIcon from "@material-ui/icons/Facebook";
 
 const Footer = () => {
-  const footerItem = (link, icon) => {
-    return (
-      <Grid item style={{ marginLeft: 20 }}>
-        <Link href={link} target="_blank" color="inherit">
-          {icon}
-        </Link>
-      </Grid>
-    );
-  };
+  const footerItems = [
+    {
+      link: "https://github.com/AndriyVeremyeyev",
+      icon: <GitHubIcon fontSize="large" />,
+    },
+    {
+      link: "https://www.linkedin.com/in/andriy-veremyeyev/",
+      icon: <LinkedInIcon fontSize="large" />,
+    },
+    {
+      link: "https://www.facebook.com/andriy.veremyeyev/",
+      icon: <FacebookIcon fontSize="large" />,
+    },
+  ];
 
   return (
     <Grid
@@ -23,19 +27,13 @@ const Footer = () => {
       justify="flex-end"
       style={{ marginTop: 50 }}
     >
-      {footerItem(
-        "https://github.com/AndriyVeremyeyev",
-        <GitHubIcon fontSize="large" />
-      )}
-      {footerItem(
-        "https://www.linkedin.com/in/andriy-veremyeyev/",
-        <LinkedInIcon fontSize="large" />
-      )}
-      {footerItem(
-        "https://www.facebook.com/andriy.veremyeyev/",
-        <FacebookIcon fontSize="large" />
-      )}
-      {footerItem("", <EmailIcon fontSize="large" />)}
+      {footerItems.map((item, index) => (
+        <Grid key={`footerItem${index}`} item style={{ marginLeft: 20 }}>
+          <Link href={item.link} target="_blank" color="inherit">
+            {item.icon}
+          </Link>
+        </Grid>
+      ))}
     </Grid>
   );
 };
