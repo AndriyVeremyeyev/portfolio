526c138cbcb12b3ba5d83bb459f0d626e9151e4c
Andriy Veremyeyev <belyybrat@gmail.com>
2021-03-19 19:36:02 -0700

    Add email validation

--- src/EmailForm.jsx
+++ src/EmailForm.jsx
@@ -16,32 +16,49 @@ const EmailForm = () => {
     message: "",
   };
   const [form, setForm] = useState(emptyForm);
-  const [showForm, setShowForm] = useState([true, false]);
+  const [showForm, setShowForm] = useState([true, false, false, false]);
+
+  const validateEmail = (email) => {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+  };
+
+  console.log(form.name.length, form.subject.length);
 
   const sendEmail = (e) => {
     e.preventDefault();
 
-    const templateParams = {
-      from_name: form.name,
-      email: form.email,
-      subject: form.subject,
-      message_html: form.message,
-    };
+    if (!validateEmail(form.email)) {
+      setShowForm([true, false, false, true]);
+    } else if (
+      !form.name.length &&
+      !form.subject.length &&
+      !form.subject.message
+    ) {
+      setShowForm([true, false, true, false]);
+    } else {
+      const templateParams = {
+        from_name: form.name,
+        email: form.email,
+        subject: form.subject,
+        message_html: form.message,
+      };
 
-    emailjs.send(
-      "service_r80rhn8",
-      "template_48uwkv8",
-      templateParams,
-      "user_qNzdA0MA8Q5xTka6tyqBP"
-    );
+      emailjs.send(
+        "service_r80rhn8",
+        "template_48uwkv8",
+        templateParams,
+        "user_qNzdA0MA8Q5xTka6tyqBP"
+      );
 
-    setForm(emptyForm);
-    setShowForm([false, true]);
+      setForm(emptyForm);
+      setShowForm([false, true, false, false]);
+    }
   };
 
   useEffect(() => {
-    console.log(form);
-  }, [form]);
+    console.log(showForm);
+  }, [showForm]);
 
   return (
     <React.Fragment>
@@ -89,6 +106,16 @@ const EmailForm = () => {
             Send
           </Button>
         </Grid>
+      </Collapse>
+      <Collapse in={showForm[3]} timeout={1000}>
+        <Typography variant="h6">
+          Make sure that you provided correct e-mail
+        </Typography>
+      </Collapse>
+      <Collapse in={showForm[2]} timeout={1000}>
+        <Typography variant="h6">
+          Make sure that you didn't left blank empty
+        </Typography>
       </Collapse>
       <Collapse in={showForm[1]} timeout={1000}>
         <Typography variant="h6">
